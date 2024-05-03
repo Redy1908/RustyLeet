f4c7ebd617b57301b87715eb007c8c35732c598b
Redy1908 <58226371+Redy1908@users.noreply.github.com>
2024-05-03 12:40:42 +0200

    Added problem 271

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -3,3 +3,4 @@ mod s0002_valid_anagram;
 mod s0003_two_sum;
 mod s0004_group_anagrams;
 mod s0005_top_k_frequent_elements;
+mod s0006_encode_and_decode_strings;
---
+++ src/solutions/s0006_encode_and_decode_strings.rs
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/encode-and-decode-strings/description/
+
+fn solution(strs: Vec<&str>) -> Vec<String> {
+    let encoded = encode(strs);
+    decode(&encoded)
+}
+
+fn encode(strs: Vec<&str>) -> String {
+    strs.join("#")
+}
+
+fn decode(str: &str) -> Vec<String> {
+    str.split("#").map(String::from).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::solution;
+
+    #[test]
+    fn should_return_true() {
+        let strs = vec!["lint", "code", "love", "you"];
+        let expected = vec!["lint", "code", "love", "you"];
+        assert_eq!(solution(strs), expected);
+    }
+}
